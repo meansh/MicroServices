1fdab9b14f83728a4821c67922e7c12d394253c2
meansh <anshdotcontact@gmail.com>
2023-03-04 18:43:29 +0530

    User Service Microservice

---
+++ src/main/java/com/lcwd/user/service/entities/User.java
@@ -0,0 +1,35 @@
+package com.lcwd.user.service.entities;
+
+
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "micro_users")
+public class User {
+    @Id
+    @Column(name = "ID")
+    private String userId;
+
+    @Column(name = "NAME", length = 20)
+    private String name;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+
+    @Column(name = "ABOUT")
+    private String about;
+
+    @Transient
+    private List<Rating> ratings;
+}
---
+++ src/main/java/com/lcwd/user/service/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.lcwd.user.service.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.lcwd.user.service.entities.User;
+public interface UserRepository extends JpaRepository<User, String>
+{
+}
